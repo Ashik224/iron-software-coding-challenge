b396ab6764b87b954b6dadca54245c8f88758ced
ANANDA-PC\ashik <ashik.cse.pstu@gmail.com>
2026-03-04 02:53:17 +0600

    feature: create the main class and methods for the implementation

---
+++ IronSoftwareCodingChallenge/OldPhonePadTextConversion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IronSoftwareCodingChallenge
+{
+    internal class OldPhonePadTextConversion
+    {
+        public static String OldPhonePad(string input)
+        {
+
+
+
+
+
+
+            return "";
+        }
+    }
+}
---
+++ IronSoftwareCodingChallenge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace IronSoftwareCodingChallenge
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

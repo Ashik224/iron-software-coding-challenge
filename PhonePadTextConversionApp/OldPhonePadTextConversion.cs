cc007fdf61bedd90d0f174a61dec370848b85618
ANANDA-PC\ashik <ashik.cse.pstu@gmail.com>
2026-03-08 03:13:04 +0600

    use StringBuilder instead of string concatenation

--- PhonePadTextConversionApp/OldPhonePadTextConversion.cs
+++ PhonePadTextConversionApp/OldPhonePadTextConversion.cs
@@ -8,20 +8,22 @@ namespace PhonePadTextConversionApp
     {
         public static String OldPhoneTextConverter(string input)
         {
-            string finalOutput = "";
+            StringBuilder builder = new();
             input = input.Trim();
 
             for (int i = 0; i < input.Length; i++)
             {
-                if (input[i] == ' ') continue;
+                if (input[i] == '#') 
+                    break;
+                if (input[i] == ' ') 
+                    continue;
                 if(input[i] == '*')
                 {
-                    if(finalOutput.Length > 0)
-                        finalOutput = finalOutput.Remove(finalOutput.Length - 1);
+                    if (builder.Length > 0)
+                        builder.Length--;
                     continue;
                 }
-                if (input[i] == '#') break;
-
+                
                 int count = 0;
                 while (input[i] == input[i+1])
                 {
@@ -33,12 +35,14 @@ namespace PhonePadTextConversionApp
                 if (input[i] == '7' || input[i] == '9')
                 {
                     rem = count % 4;
-                    if (rem == 0) rem = 4;
+                    if (rem == 0) 
+                        rem = 4;
                 }
                 else
                 {
                     rem = count % 3;
-                    if (rem == 0) rem = 3;
+                    if (rem == 0) 
+                        rem = 3;
                 }
 
                 int digit = input[i] - '0';
@@ -46,8 +50,10 @@ namespace PhonePadTextConversionApp
                 if(digit == 8 || digit == 9)
                     alphabet += (char) 1;
 
-                finalOutput += alphabet;
-            }           
+                builder.Append(alphabet);
+            }          
+            string finalOutput = builder.ToString();
+
             return finalOutput;
         }
     }
--- PhonePadTextConversionApp/Program.cs
+++ PhonePadTextConversionApp/Program.cs
@@ -7,7 +7,7 @@ namespace IronSoftwareCodingChallenge
         private static void Main(string[] args)
         {
             string output = OldPhonePadTextConversion.OldPhoneTextConverter("8 88777444666*664#");
-            Console.WriteLine("Final string: " +output);
+            Console.WriteLine(output);
         }
     }
 }
